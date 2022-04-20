e1887e093626d8f8898993cda61f66eda168077a
Badal kumar <88502214+Badaligc@users.noreply.github.com>
2022-04-20 23:03:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var str = "Skill Safari"
+
+// lenth of the string
+console.log(str.length)
+
+//Convert all letter in string into upper case.
+const first = "skill safari"
+const upper = first.toUpperCase()
+console.log(upper)
+
+//index position of a
+console.log(str.indexOf("a"))
+// display a famous conversation between captain america and iron man 
+console.log("Captain America")
+console.log("Iron man")
+
+//Create BMI
+let height = 2
+const weight = 40
+var BMI
+
+BMI =   weight / (height * height)
+console.log(BMI)
+
+
+
